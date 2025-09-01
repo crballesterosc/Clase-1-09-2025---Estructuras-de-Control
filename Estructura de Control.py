2c344d3053eb56537ac6e27d0231985b7b2e6046
Your Name <your_email@example.com>
2025-09-01 12:49:02 -0500

    Ejercicio - 1/09/2025

---
+++ Estructura de Control.py
@@ -0,0 +1,50 @@
+A = int("10")
+B = int("6")
+C = int("65")
+D = float("3.15")
+E = int("-9")
+F = int("38")
+
+
+if(A > 5):
+    print("Este es el resultado del if verdadero")
+    print(f"La variable A es igual a: {A}")
+else: 
+    print("La condicion no se ejecuto")
+print()
+
+if(B < 20):
+    print("Este es el resultado del if verdadero")
+    print(f"La variable B es igual a: {B}")
+else: 
+    print("La condicion no se ejecuto")
+print()
+
+if(C == 65):
+   print("Este es el resultado del if verdadero")
+   print(f"La variable C es igual a: {C}") 
+else: 
+    print("La condicion no se ejecuto")
+print()
+
+if(D >= 3.10):
+   print("Este es el resultado del if verdadero")
+   print(f"La variable D es igual a: {D}") 
+else: 
+    print("La condicion no se ejecuto")
+print()
+
+if(E <= -50):
+   print("Este es el resultado del if verdadero")
+   print(f"La variable E es igual a: {E}") 
+else: 
+    print("La condicion no se ejecuto")
+print()
+
+if(F != 39):
+   print("Este es el resultado del if verdadero")
+   print(f"La variable F es igual a: {F}") 
+else: 
+    print("La condicion no se ejecuto")
+print()
+
